30b895adfb2df2c476bb0fc61a8eaec5b4674a79
eychoi11 <eliseychoi@gmail.com>
2025-01-28 00:42:45 -0800

    test

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
 
 (function() {
     window.addEventListener('load', init);
+    document.addEventListener("DOMContentLoaded", function(event) { 
+        alert('Hey')
+      });
     let currentGuess = 1;
-    
+    window.onload = (event) => {
+        console.log('test')
+    };
     function init() {
         console.log('in init');
         document.getElementById('#submit').addEventListener('click', submitGuess);
@@ -15,5 +20,9 @@
         let word = userInputElement.innerText;
         console.log('word: ' + word);
         userInputElement.innerText = '';
+    }
+
+    function test() {
+        console.log('test')
     }
 })
