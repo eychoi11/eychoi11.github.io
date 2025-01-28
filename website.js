85f82b30a4d15382fe3dbfc9cddf4071f082d458
eychoi11 <eliseychoi@gmail.com>
2025-01-27 23:31:42 -0800

    test

--- website.js
+++ website.js
@@ -2,8 +2,14 @@
 
 (function() {
     window.addEventListener('load', init);
+    let currentGuess = 1;
 
     function init() {
-        
+        document.getElementById('#submit').addEventListener('click', submitGuess());
+    }
+
+    function submitGuess() {
+        let word = document.getElementById('#user-input').innerText;
+        console.log('word: ' + word);
     }
 })
